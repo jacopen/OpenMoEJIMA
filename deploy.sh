e660e55b66733697c270d1237ef995de82cdbd60
u1 <u1@eternus.udcp.local>
2010-01-01 22:24:24 +0900

    moejima.shが不必要にした

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,5 @@ cp -R moejimabot.nb $NADOKA_DIR/plugins/
 nadoka_pid=`ps aux | grep nadoka | egrep -v grep | awk '{print \$2}'`
 kill -9 $nadoka_pid
 cd $NADOKA_DIR
-$NADOKA_DIR/moejima.sh
+ruby $NADOKA_DIR/nadoka.rb --r $NADOKA_DIR/nadoka_config_main & 
+#$NADOKA_DIR/moejima.sh
